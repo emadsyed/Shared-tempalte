b086f6a7783020e3277ea751358c41cb56441897
Adil <adilforms@gmail.com>
2018-12-07 12:12:22 -0500

    Adding tempalte

--- vars/template.groovy
+++ vars/template.groovy
@@ -1,29 +1,46 @@
-/**
- * Defines a pipeline template (as a sample with one job parameter 
- * that should be common for all pipelines)
- */
-def createMyStandardDeclarativePipeline(Map templateParams) {   
 
+#!/usr/bin/env groovy
+def call(Map pipelineParams) {
     pipeline {
-        agent any
-        parameters {
-            string(name: 'myInput', description: 'Some pipeline parameters')
-        }
-        stages {
-            stage('Stage one') {
-                steps {
-                    script {
-                        echo "Parameter from template creation: " + templateParams.someParam
-                    }
-                }
-            }
-            stage('Stage two') {
-                steps {
-                    script {
-                        echo "Job input parameter: " + params.myInput
-                    }
-                }
-            }
-        }
-    }
+  agent any
+stages {
+stage('Checkout'){
+  steps{
+/*
+      checkout([$class: 'GitSCM',
+            branches: [[name: env.BRANCH_NAME]],
+           extensions: [[$class: 'CleanBeforeCheckout']],
+                         userRemoteConfigs: [[url: env.REPO_NAME]] 
+                        ])
+  */
+      checkout([$class:'GitSCM', branches: "**"])
+  }
 }
+stage('Build'){
+  steps {
+    echo 'building'
+    sh 'npm install'
+  }
+}
+stage('Test'){ steps {
+    echo 'Testing'
+  
+  }
+  
+}
+stage('Publish') {
+  steps {
+    
+    sh '''#!/bin/bash -el
+    echo 'publishing'
+   
+    docker build -t adilforms/the-example-app.nodejs .
+         docker login --username adilforms --password Rimsha@548
+    docker push adilforms/the-example-app.nodejs   
+    '''
+  }
+}}
+}
+
+}
+
