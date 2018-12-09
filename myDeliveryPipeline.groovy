bafec7c759f57e7ab59678d495e94659531104aa
emadsyed <44439192+emadsyed@users.noreply.github.com>
2018-12-09 18:07:15 -0500

    Removed Publish Step

--- myDeliveryPipeline.groovy
+++ myDeliveryPipeline.groovy
@@ -31,13 +31,9 @@ stage('Test'){ steps {
 stage('Publish') {
   steps {
     
-    sh '''#!/bin/bash -el
+ 
     echo 'publishing'
    
-    docker build -t adilforms/the-example-app.nodejs .
-         docker login --username adilforms --password Rimsha@548
-    docker push adilforms/the-example-app.nodejs   
-    '''
   }
 }}
 }
